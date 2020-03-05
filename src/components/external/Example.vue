9f344af7f3679d50b4b1372183708685ac18f853
Arnaud Marchand <amarchand@icloud.com>
2020-03-05 14:33:54 +0100

    Save point

---
+++ src/components/external/Example.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="width: 100%">
+    
+    </div>
+</template>
+<script>
+import Vue from "vue";
+import axios from "axios";
+
+export default {
+  name: "Example",
+  data: () => ({}),
+  props: {
+    config: {
+      type: Object
+    }
+  },
+  created: function() {
+  },
+  methods: {
+  }
+};
+
+</script>
+<style>
+
+</style>
