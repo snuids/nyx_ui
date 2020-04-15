d5452809075dec004f54f25e6f39cabe91fdeec2
Arnaud Marchand <amarchand@icloud.com>
2020-04-15 13:06:41 +0200

    Version bumped

--- src/store/store.js
+++ src/store/store.js
@@ -54,7 +54,7 @@ export default new Vuex.Store({
     apiurl: "api/v1/",
     apiVersion: "",
     kibanaurl: "/kibana/",
-    version: "v3.18.1",
+    version: "v3.19.0",
     devMode: false,
     menus: [],
     menuOpen: true,
