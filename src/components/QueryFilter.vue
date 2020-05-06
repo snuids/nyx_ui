9cd2cf01853b91d5127b77ef2af760f5d6763d20
lapin <va.mercier@gmail.com>
2020-05-06 13:50:15 +0200

    Update QueryFilter.vue

--- src/components/QueryFilter.vue
+++ src/components/QueryFilter.vue
@@ -43,6 +43,7 @@
 
         <el-input
           @change="refresh()"
+          clearable
           size="mini"
           style="width:170px"
           v-if="queryfilter.type == 'text' || queryfilter.type == 'text_strict'"
