95ec013aa2f96a02cc2df789ee2d64aec9112555
Arnaud Marchand <amarchand@icloud.com>
2026-04-12 14:52:28 +0200

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -53,7 +53,7 @@ export default new Vuex.Store({
     apiurl: "api/v1/",
     apiVersion: "",
     kibanaurl: "/kibana/",
-    version: "v3.28.1",
+    version: "v3.28.2",
     devMode: false,
     menus: [],
     menuOpen: true,
